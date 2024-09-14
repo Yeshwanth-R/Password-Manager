ef5607375858d76404a807b464f0baf713e21356
YESHWANTH R <eng23cs0234@dsu.edu.in>
2024-09-14 11:59:54 +0530

    First Commit

---
+++ app/page.js
@@ -0,0 +1,16 @@
+import Body from "@/components/Body";
+import Footer from "@/components/Footer";
+import NavBar from "@/components/NavBar";
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <div>
+        <NavBar />
+        <Body />
+        <Footer />
+      </div>
+    </>
+  );
+}
---
+++ components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-slate-800 py-3 text-white flex flex-col gap-3 justify-center items-center">
+      <div>
+        <span className="font-bold text-green-700">&lt;</span>
+        Pass
+        <span className="font-bold text-green-700">OP/&gt;</span>
+      </div>
+      <div>Created By Yeshwanth R ❤️</div>
+    </div>
+  );
+};
+
+export default Footer;
